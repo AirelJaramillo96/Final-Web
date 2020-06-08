dcd1d05feb5b1893471a6234a914902f5c8c34f4
AirelJaramillo96 <airel.jaramillo@cceo.com.mx>
2020-06-07 23:29:11 -0500

    User and branch controller

---
+++ app/Http/Controllers/BranchController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Branch;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class BranchController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Branch
+     */
+    public function store(Request $request)
+    {
+        $branch = new Branch();
+        $branch->fill($request->all());
+        $branch->password = bcrypt('12345678');
+        $branch->save();
+
+        return $branch;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Branch::findOrfail($id);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $branch = Branch::findOrfail($id);
+        $branch->fill($request->all());
+
+        $branch->save();
+
+        return $branch;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $branch = User::findOrfail($id);
+        $branch->delete();
+
+        return $branch;
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return User
+     */
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->fill($request->all());
+        $user->password = bcrypt('12345678');
+        $user->save();
+
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::findOrfail($id);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrfail($id);
+        $user->fill($request->all());
+
+        $user->save();
+
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrfail($id);
+        $user->email = $user->email . "_" . $user->id;
+        $user->save();
+        $user->delete();
+
+        return $user;
+    }
+}
